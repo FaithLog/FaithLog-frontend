fc22dcf7e6fd6b2604259c8a5fb870ccc77bcaf6
josephuk77 <69853298+josephuk77@users.noreply.github.com>
2026-07-27 17:05:48 +0900

    perf(android): enable full R8 optimization (#213)

--- plugins/withAndroidReleaseRecommendations.js
+++ plugins/withAndroidReleaseRecommendations.js
@@ -1,8 +1,15 @@
 const {
   AndroidConfig,
   withAndroidManifest,
+  withAppBuildGradle,
+  withGradleProperties,
   withAndroidStyles,
 } = require('@expo/config-plugins');
+
+const OPTIMIZED_PROGUARD_FILE = 'proguard-android-optimize.txt';
+const LEGACY_PROGUARD_FILE_PATTERN =
+  /getDefaultProguardFile\((["'])proguard-android\.txt\1\)/g;
+const OPTIMIZED_RESOURCE_SHRINKING_KEY = 'android.r8.optimizedResourceShrinking';
 
 const DEPRECATED_SYSTEM_BAR_ITEMS = new Set([
   'android:statusBarColor',
@@ -11,6 +18,51 @@ const DEPRECATED_SYSTEM_BAR_ITEMS = new Set([
   'android:enforceStatusBarContrast',
   'android:enforceNavigationBarContrast',
 ]);
+
+function enableR8OptimizingDefaults(contents) {
+  if (contents.includes(OPTIMIZED_PROGUARD_FILE)) {
+    return contents;
+  }
+
+  if (!LEGACY_PROGUARD_FILE_PATTERN.test(contents)) {
+    throw new Error(
+      'Unable to enable R8 optimization: Expo Android release ProGuard configuration was not found.',
+    );
+  }
+
+  LEGACY_PROGUARD_FILE_PATTERN.lastIndex = 0;
+  return contents.replace(
+    LEGACY_PROGUARD_FILE_PATTERN,
+    'getDefaultProguardFile("proguard-android-optimize.txt")',
+  );
+}
+
+function enableOptimizedResourceShrinking(properties) {
+  const nextProperties = properties.filter(
+    (property) =>
+      property.type !== 'property' || property.key !== OPTIMIZED_RESOURCE_SHRINKING_KEY,
+  );
+
+  nextProperties.push({
+    type: 'property',
+    key: OPTIMIZED_RESOURCE_SHRINKING_KEY,
+    value: 'true',
+  });
+
+  return nextProperties;
+}
+
+function withR8Optimizations(config) {
+  const withOptimizedDefaults = withAppBuildGradle(config, (config) => {
+    config.modResults.contents = enableR8OptimizingDefaults(config.modResults.contents);
+    return config;
+  });
+
+  return withGradleProperties(withOptimizedDefaults, (config) => {
+    config.modResults = enableOptimizedResourceShrinking(config.modResults);
+    return config;
+  });
+}
 
 function withResizableMainActivity(config) {
   return withAndroidManifest(config, (config) => {
@@ -36,7 +88,11 @@ function withEdgeToEdgeTheme(config) {
 }
 
 function withAndroidReleaseRecommendations(config) {
-  return withEdgeToEdgeTheme(withResizableMainActivity(config));
+  return withR8Optimizations(withEdgeToEdgeTheme(withResizableMainActivity(config)));
 }
 
 module.exports = withAndroidReleaseRecommendations;
+module.exports.__internal = {
+  enableOptimizedResourceShrinking,
+  enableR8OptimizingDefaults,
+};
